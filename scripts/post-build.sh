38afb823fab6cf74d373436d18270054d6032722
Christian Mahnke <cmahnke@gmail.com>
2021-04-15 22:42:52 +0200

    Updated post build cleanup

--- scripts/post-build.sh
+++ scripts/post-build.sh
@@ -3,5 +3,6 @@
 # Indexing
 node scripts/build-index.js
 
-find  docs/post -type d -mindepth 2 -exec rm -rf {} \;
+find docs/post -type d -mindepth 2 -exec rm -rf {} \;
 find docs/ -name 'index.json' -exec rm {} \;
+rm -rf docs/tags/*
