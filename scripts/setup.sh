149bd9276751da011127025706d9d2a9972cdc44
Christian Mahnke <cmahnke@gmail.com>
2021-04-20 19:54:20 +0200

    Try to fix CSS

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,12 +5,8 @@ convert -resize 1024x1024 -transparent white -density 600 "Source Files/Logo/Lab
 
 # Favicons
 # See https://gist.github.com/pfig/1808188
-convert static/images/cm.svg static/images/cm.png
-convert static/images/cm.png -resize 128x128 -transparent white static/images/favicon-128.png
-convert static/images/favicon-128.png -resize 16x16 static/images/favicon-16.png
-convert static/images/favicon-128.png -resize 32x32 static/images/favicon-32.png
-convert static/images/favicon-128.png -resize 64x64 static/images/favicon-64.png
-convert static/images/favicon-16.png static/images/favicon-32.png static/images/favicon-64.png static/images/favicon-128.png -colors 256 static/images/favicon.ico
+
+SOURCE="theme/projektemacher-base/images/cm.png" OPTIONS="-transparent white static/images/favicon-128.png" ./themes/projektemacher-base/scripts/favicon.sh
 
 # IIIF tiles
 echo "Set SKIP_IIIF to something to disable generation of IIIF derivates"
