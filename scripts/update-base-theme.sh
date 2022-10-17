bee17848181f95706e6168965be3c40c055d2a95
Christian Mahnke <cmahnke@gmail.com>
2022-10-17 07:58:55 +0200

    update the other common base theme as well

--- scripts/update-base-theme.sh
+++ scripts/update-base-theme.sh
@@ -10,4 +10,10 @@ do
     git pull origin main
     cd ../../../..
   fi
+  if [ -d blogs/$BLOG/themes/observer-hugo ] ; then
+    cd blogs/$BLOG/themes/observer-hugo
+    echo "Updating 'observer-hugo' theme for ${BLOG}"
+    git pull origin main
+    cd ../../../..
+  fi
 done
