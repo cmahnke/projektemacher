b5aceb9e9ac5fadb30d94659d03ef5c68f4b457c
Christian Mahnke <cmahnke@gmail.com>
2024-02-21 12:38:49 +0100

    Fixed Cleanup

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,11 +2,12 @@
 
 for BLOG in `ls -1 blogs`
 do
-    if [ -x "$BLOG/scripts/cleanup.sh" ] ; then
+    if [ -x "blogs/$BLOG/scripts/cleanup.sh" ] ; then
         echo "Cleaning $BLOG"
-        $BLOG/scripts/cleanup.sh
+        blogs/$BLOG/scripts/cleanup.sh
         $(cd blogs/$BLOG && git branch -d gh-pages)
     fi
 done
 
+./themes/projektemacher-base/scripts/cleanup.sh
 rm -rf docs/* resources/_gen/* static/images/favicon*
