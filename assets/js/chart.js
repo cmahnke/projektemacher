3a484b4bf1a5bc8181939024f9cc6a78e9f99376
Christian Mahnke <cmahnke@gmail.com>
2025-01-05 12:54:18 +0100

    try echarts

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -1,34 +1,76 @@
 // See https://observablehq.com/@d3/stacked-to-grouped-bars
-import { bb } from 'billboard.js';
+//import { bb, bar } from 'billboard.js';
+import * as echarts from 'echarts';
 
-//TODO: This is currently just some example code
+const defaultHeightVH = 50;
+
+async function loadCSVFromURL(url) {
+  try {
+    const response = await fetch(url);
+    const text = await response.text();
+    const lines = text.split('\n');
+    const headers = lines[0].split(',');
+    const data = [];
+
+    for (let i = 1; i < lines.length; i++) {
+      const row = lines[i].split(',');
+      data.push(row);
+    }
+
+    //return data[0].map((_, i) => data.map(row => row[i]));
+    return data;
+  } catch (error) {
+    console.error('Error loading CSV from URL:', error);
+    return []; // Return an empty array on error
+  }
+}
+
 function chart(csvFile, element) {
-  d3.csv(csvFile).then(function(data) {
-    const columns = Object.keys(data[0]);
+  if (element.startsWith('#')) {
+    element = element.substring(1);
+  }
+  loadCSVFromURL(csvFile).then(data => {
+    const container = document.getElementById(element)
+    var width = container.offsetWidth;
+    if (width === 0) {
+      width = container.parentElement.parentElement.offsetWidth;
+    }
+    var height = container.offsetHeight;
+    if (height === 0) {
+      height = Math.round(window.innerHeight / 100) * defaultHeightVH;
+    }
+    var myChart = echarts.init(container, null, { renderer: "svg", width: width, height: height});
 
-    const chartData = {
-      columns: [
-        ['x'].concat(data.map(d => d[Object.keys(data[0])[0]])),
-        ...columns.map(col => [col].concat(data.map(d => +d[col])))
-      ],
-      type: 'bar',
-      groups: [columns]
-    };
-
-    bb.generate({
-      bindto: element,
-      data: chartData,
-      bar: {
-        width: {
-          ratio: 0.8
-        }
+    var option = {
+      dataset:{
+        source:data,
+        dimensions: ['date', 'blog', 'title'],
       },
-      axis: {
-        x: {
-          type: 'category'
+      series: [
+        {
+          name: 'blog',
+          type: 'line',
+          encode: {
+            x: 'timestamp',
+            y: 'blog'
+          }
         }
-      }
-    });
+      ],
+      title: {
+      },
+      tooltip: {},
+      /*
+      legend: {
+        data: ['sales']
+      },
+      */
+      xAxis: {
+      },
+      yAxis: {},
+
+    };
+    myChart.setOption(option);
+
   });
 }
 
