896e9b5513895cc9bad9d12548e603141d5d6e57
Christian Mahnke <cmahnke@gmail.com>
2025-04-20 16:51:05 +0200

    Added Hugo and submodules

--- scripts/update-blogs.sh
+++ scripts/update-blogs.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
+COMPACT=$1
+
 #git submodule init
 
 for BLOG in `ls -1 blogs`
 do
   echo "-> Updating $BLOG"
   cd blogs/$BLOG
-  git pull origin main
+  if [ -n "$COMPACT" ] ; then
+    git fetch --depth=1 origin main
+    git reflog expire --expire-unreachable=now --all
+    git gc --aggressive --prune=all
+  else
+    git pull origin main
+  fi
   cd ../..
   git add blogs/$BLOG
 done
