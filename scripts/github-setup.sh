c3a2982ac6ff633261d34a5ef27abb861f2e1e15
cmahnke <194820+cmahnke@users.noreply.github.com>
2025-11-04 09:43:24 +0100

    Comment out setup-yarn.sh in github-setup.sh
    Comment out the setup-yarn.sh script call in github-setup.sh.

--- scripts/github-setup.sh
+++ scripts/github-setup.sh
@@ -3,6 +3,5 @@
 CTX_PATH="$(dirname $(realpath $0))"
 
 $CTX_PATH/../themes/projektemacher-base/scripts/github/setup-dependencies.sh
-#$CTX_PATH/../themes/projektemacher-base/scripts/github/setup-yarn.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/docker-images.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/dart-sass.sh
